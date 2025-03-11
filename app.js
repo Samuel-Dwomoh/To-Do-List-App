e1561dff8de7210e8aef6bfeb5478f8379964f80
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-03-11 14:02:52 +0000

    Updated

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const box = document.getElementById("box");
               alert("You must type something!");
           } else {
               let li = document.createElement("li");
-              li.className = "bg-gray-200 my-2 pl-4 pr-2 py-4 hover:cursor-pointer break-words whitespace-normal rounded-xl font-mono italic";
+              li.className = "bg-gray-200 my-2 pl-4 pr-2 py-4 flex justify-center items-center hover:cursor-pointer break-words whitespace-normal rounded-xl font-mono italic shadow";
             li.addEventListener("click", function () {
             li.classList.toggle("line-through");
             });
 
               li.innerHTML = `<span>${box.value}</span>`;
               let span = document.createElement("span");
-              span.innerHTML = "\u00D7";
-              span.className = "text-red-600 pl-4 right-0 font-bold cursor-pointer hover:text-red-800";
+              span.innerHTML = "X";
+              span.className = "w-4 h-4 flex items-center justify-center bg-black text-white rounded-full ml-4 hover:bg-red-600 transition duration-200";
               span.onclick = function() {
                   li.remove();
               };
@@ -26,7 +26,7 @@ const box = document.getElementById("box");
         }
         box.addEventListener("keypress", function(event) {
             if (event.key === "Enter") { 
-                addTask(); // Call the function when Enter is pressed
+                addTask();
             }
         });
         
