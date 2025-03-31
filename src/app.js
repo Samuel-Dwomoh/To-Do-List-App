b04f7423433c8b11dcc996e4acbe905b19abb089
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-03-31 23:25:02 +0000

    new

--- src/app.js
+++ src/app.js
@@ -11,38 +11,38 @@ function addTask() {
     } else {
         let li = document.createElement("li");
         li.className = "bg-gray-200 my-2 pl-4 pr-2 py-4 flex justify-between items-center hover:cursor-pointer break-words whitespace-normal rounded-xl font-mono italic shadow";
-        
+
         li.addEventListener("mouseenter", () => {
             let tooltip = document.createElement("div");
             tooltip.innerText = "Click to mark as done!";
             tooltip.className = "absolute bg-black text-white text-xs px-2 rounded-md transform -translate-y-2";
             li.appendChild(tooltip);
         });
-        
+
         li.addEventListener("mouseleave", () => {
             let tooltip = li.querySelector("div");
             if (tooltip) {
                 tooltip.remove();
             }
         });
-        
+
         li.addEventListener("click", () => {
             li.classList.toggle("line-through");
         });
-        
+
         li.innerHTML = `<span>${box.value}</span>`;
 
         let span = document.createElement("span");
         span.className = "w-6 h-6 ml-1";
 
         let animation = lottie.loadAnimation({
-            container: span, 
+            container: span,
             renderer: "svg",
             loop: true,
             autoplay: true,
             path: "/Cross.json"
         });
-        
+
         span.onclick = function () {
             li.remove();
         };
@@ -64,7 +64,7 @@ head.classList.add("hover:cursor-pointer");
 dark.addEventListener("click", () => {
     /*Ading dark mode feature here soon*/
     alert("Dark mode coming soon!");
-    
+
 })
 
 window.addTask = addTask;
